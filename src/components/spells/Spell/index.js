8ab81ae2ffec11a68868282008c215367a5f1446
Leo <leogen17@gmail.com>
2018-01-25 21:27:48 -0500

    Add support for spell info to display

--- src/components/spells/Spell/index.js
+++ src/components/spells/Spell/index.js
@@ -3,12 +3,18 @@ import { h, Component } from 'preact';
 class Spell extends Component {
   render () {
     return (
-      <p
-        class='spell'
+      <div
+        class='spell has-shadow'
         onClick={() => this.props.removeSpellFromTome(this.props.name)}
       >
-        {this.props.name}
-      </p>
+        <p>{this.props.name}</p>
+        <p>{this.props.castingTime}</p>
+        <p>{this.props.classes}</p>
+        <p>{this.props.description}</p>
+        <p>{this.props.duration}</p>
+        <p>{this.props.range}</p>
+        <p>{this.props.school}</p>
+      </div>
     );
   }
 }
