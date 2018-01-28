15601ee0b003472e223577690ad946cb94a01b69
Leo Generali <leogen17@gmail.com>
2018-01-28 15:38:28 -0500

    Add support for icons

--- src/components/spells/Spell/index.js
+++ src/components/spells/Spell/index.js
@@ -1,6 +1,7 @@
 import { h, Component } from 'preact';
 
 import SpellComponents from '../SpellComponents';
+import DeleteIcon from '../../shared/Icons/Delete';
 
 class Spell extends Component {
   state = {
@@ -24,19 +25,21 @@ class Spell extends Component {
         <div class='spell__row'>
           <p class='text text--bold spell__name'>{this.props.name}</p>
           <SpellComponents components={this.props.components}/>
-          <button class='btn' onClick={() => this.props.removeSpellFromTome(this.props.name)}>Del</button>
+          <p class='text text--regular spell__range'>{this.props.range}</p>
+          <p class='text text--regular spell__casting-time'>{this.props.castingTime}</p>
+          <p class='text text--regular spell__duration'>{this.props.duration}</p>
+          <button class='btn icon__delete' onClick={() => this.props.removeSpellFromTome(this.props.name)}>
+            <DeleteIcon />
+          </button>
         </div>
-        <p class='text text--regular spell__casting-time'>{this.props.castingTime}</p>
-        <p class='text text--regular spell__classes'>{this.props.classes}</p>
         <div class='spell__description-container'>
           <svg class={`nav-indicator ${showingDescription.icon}`} width='22' height='22' viewbox='0 0 26 26' onClick={() => this.showDescription()}>
             <path d='M11,1 L21,11 L11,21 L1,11'></path>
           </svg>
           <p class={`text text--regular spell__description ${showingDescription.description}`}>{this.props.description}</p>
+          <p class={`text text--regular spell__school ${showingDescription.description}`}>{`School: ${this.props.school}`}</p>
+          <p class={`text text--regular spell__classes ${showingDescription.description}`}>{this.props.classes}</p>
         </div>
-        <p class='text text--regular spell__duration'>{this.props.duration}</p>
-        <p class='text text--regular spell__range'>{this.props.range}</p>
-        <p class='text text--regular spell__school'>{this.props.school}</p>
       </article>
     );
   }
