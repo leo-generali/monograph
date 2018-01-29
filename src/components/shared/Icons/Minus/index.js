b152eee97d43ac1b346a66a6beb49d8fecfabfff
Leo <leogen17@gmail.com>
2018-01-28 21:10:24 -0500

    Update sizing issues on spell slot icons

---
+++ src/components/shared/Icons/Minus/index.js
@@ -0,0 +1,9 @@
+import { h } from 'preact';
+
+const MinusIcon = () => (
+  <svg xmlns="http://www.w3.org/2000/svg" width="12" height="24" viewBox="0 0 24 24">
+    <path d="M0 9h24v6H0z" />
+  </svg>
+);
+
+export default MinusIcon;
---
+++ src/components/shared/Icons/Plus/index.js
@@ -0,0 +1,9 @@
+import { h } from 'preact';
+
+const PlusIcon = () => (
+  <svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" viewBox="0 0 24 24">
+    <path d="M24 9h-9v-9h-6v9h-9v6h9v9h6v-9h9z" />
+  </svg>
+);
+
+export default PlusIcon;
